9d655edb95e2cd8ea9b0fc2219ce715f39517203
Bryan Stine <stinebd@xdandroid.com>
2011-02-04 21:38:42 -0500

    Add ext2 support to mkuserimg.sh
    Change-Id: Idd065906be744239e44de5594849f642386c0ce5

--- ext4_utils/mkuserimg.sh
+++ ext4_utils/mkuserimg.sh
@@ -2,6 +2,8 @@
 #
 # To call this script, make sure make_ext4fs is somewhere in PATH
 
+PATH="out/host/${HOST_OS}-${HOST_ARCH}/bin:${PATH}:/sbin:/usr/sbin"
+
 function usage() {
 cat<<EOT
 Usage:
@@ -28,21 +30,46 @@ MOUNT_POINT=$4
 SIZE=$5
 
 case $EXT_VARIANT in
-  ext4) ;;
+  ext2)
+    num_blocks=`du -sk $SRC_DIR | tail -n1 | awk '{print $1;}'`
+
+    if [ $num_blocks -lt 20480 ]
+    then
+        extra_blocks=3072
+    else
+        extra_blocks=20480
+    fi
+
+    num_blocks=`expr $num_blocks + $extra_blocks`
+    num_inodes=`find $SRC_DIR | wc -l` ; num_inodes=`expr $num_inodes + 500`
+
+    echo genext2fs -a -d $SRC_DIR -b $num_blocks -N $num_inodes -m 0 $OUTPUT_FILE
+    genext2fs -a -d $SRC_DIR -b $num_blocks -N $num_inodes -m 0 $OUTPUT_FILE
+    echo tune2fs -L $MOUNT_POINT $OUTPUT_FILE
+    tune2fs -L $MOUNT_POINT $OUTPUT_FILE
+    echo tune2fs -C 1 $OUTPUT_FILE
+    tune2fs -C 1 $OUTPUT_FILE
+    echo e2fsck -fy $OUTPUT_FILE
+    e2fsck -fy $OUTPUT_FILE
+    if [ $? -gt 1 ] ; then
+        exit 4
+    fi
+    ;;
+  ext4)
+    if [ -z $MOUNT_POINT ]; then
+      echo "Mount point is required"
+      exit 2
+    fi
+
+    if [ -z $SIZE ]; then
+        SIZE=128M
+    fi
+
+    echo "make_ext4fs -l $SIZE -a $MOUNT_POINT $OUTPUT_FILE $SRC_DIR"
+    make_ext4fs -s -l $SIZE -a $MOUNT_POINT $OUTPUT_FILE $SRC_DIR
+    if [ $? -ne 0 ]; then
+      exit 4
+    fi
+    ;;
   *) echo "Only ext4 is supported!"; exit 3 ;;
 esac
-
-if [ -z $MOUNT_POINT ]; then
-  echo "Mount point is required"
-  exit 2
-fi
-
-if [ -z $SIZE ]; then
-    SIZE=128M
-fi
-
-echo "make_ext4fs -l $SIZE -a $MOUNT_POINT $OUTPUT_FILE $SRC_DIR"
-make_ext4fs -s -l $SIZE -a $MOUNT_POINT $OUTPUT_FILE $SRC_DIR
-if [ $? -ne 0 ]; then
-  exit 4
-fi
